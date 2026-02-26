b1e13cde34d46624d41eeb0eefad1b2e4f582549
mericerkosun <meric.erkosun@gmail.com>
2026-02-26 12:52:10 +0300

    test(parabank): refactor invalid login test to handle system bypass behavior and update docs

--- src/test/java/com/merkosun/pages/parabank/LoginPage.java
+++ src/test/java/com/merkosun/pages/parabank/LoginPage.java
@@ -1,4 +1,32 @@
 package com.merkosun.pages.parabank;
 
-public class LoginPage {
+import com.merkosun.base.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage extends BasePage {
+
+    private static final By USERNAME_INPUT = By.name("username");
+    private static final By PASSWORD_INPUT = By.name("password");
+    private static final By LOGIN_BUTTON = By.xpath("//input[@value='Log In']");
+    private static final By ERROR_MESSAGE = By.cssSelector("p.error");
+    private static final By SUCCESS_HEADER = By.xpath("//h1[contains(text(), 'Accounts Overview')]");
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void login(String username, String password) {
+        typeToElement(USERNAME_INPUT, username);
+        typeToElement(PASSWORD_INPUT, password);
+        clickToElement(LOGIN_BUTTON);
+    }
+
+    public String getErrorMessage() {
+        return getTextFromElement(ERROR_MESSAGE);
+    }
+
+    public boolean isLoginSuccessful() {
+        return isElementVisible(SUCCESS_HEADER);
+    }
 }
